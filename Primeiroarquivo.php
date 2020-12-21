4c2f873280a38809c21ed5b0e2c52e448c5e2852
Rodrigo Batista <rodrigo.batista48@etec.sp.gov.br>
2020-12-21 16:22:00 -0300

    conclui o primeiro Script php "Ola,Mundo"

--- Primeiroarquivo.php
+++ Primeiroarquivo.php
@@ -2,9 +2,20 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>Documento sem título</title>
+<title>Primeiro exemplo PHP</title>
+	<style>
+		h2{
+			color: #055F12;
+			text-shadow: 1px 1px 1px black;
+		}
+	
+	</style>
 </head>
 
 <body>
+	<h1>Testando o PHP</h1>
+	<?php
+		echo"<h2>Olá,<br>Mundo!</h2>"
+	?>
 </body>
 </html>
