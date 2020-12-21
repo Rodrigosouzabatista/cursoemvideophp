03ea53a611e4cad7632312c6c572d82e58f72dc9
Rodrigo Batista <rodrigo.batista48@etec.sp.gov.br>
2020-12-21 19:04:44 -0300

    Criei a pasta do curso em video, e fiz a segunda aula pratica nome:tipos.php

---
+++ CursoemVideo/tipos.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+	<link rel="stylesheet" href="_css/estilo.css"
+<title>Documento sem título</title>
+</head>
+<body>
+	<div>
+	<?php
+		$idade = 36;
+		$nome = "Rodrigo";
+		echo "$nome tem $idade anos"
+	
+	?>
+</div>
+</body>
+</html>
