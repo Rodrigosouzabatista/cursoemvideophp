917aa859900e860d93b59c656ff0afa86babc45b
Rodrigo Batista <rodrigo.batista48@etec.sp.gov.br>
2020-12-30 23:49:20 -0300

    iniciei o formulario da aula 08 php com html.

--- CursoemVideo/Aula 08/Modelo de Formulario.php
+++ CursoemVideo/Aula 08/Modelo de Formulario.php
@@ -8,7 +8,7 @@
 </head>
 
 <body>
-	<form>
+	<form method="post" id="contato" action="">
 	<fieldset id="usuario"><legend>Identificação do Usuário</legend>
 		<p><label for="nome">nome:</label><input type="text" name="nome" id="nome" size= "20" maxlenght="30" placeholder="Nome Completo"/></p>
 		<p><label for="senha">senha:</label><input type="password" name="senha" id="senha" size= "3" maxlength="8" placeholder="8 dígitos"/></p>
@@ -16,7 +16,33 @@
 		<fieldset id= "sexo"><legend>sexo:</legend>
 			<input type="radio" name="sexo" id="masc"/> <label for="masc">masculino</label><br/>
 			<input type="radio" name="sexo" id="fem"/><label for="fem">feminino</label></fieldset>
-		<p>data de nascimento:</p>
+		<p><label for="data">data de nascimento:</label><input type="date" name="data" id="data"/></p>
+		
+		como fazer formulario de estado
+		<p><label for="estado">Estado:</label>
+		<select name="estado" id="estado">
+		<optgroup label="Região Sudeste">
+			<option value="RJ">Rio de Janeiro</option>
+			<option value="SP" selected>São Paulo</option>
+			<option value="MG">Minas Gerais</option>
+		</optgroup>
+			<optgroup label="Região Sul">
+			<option value="PR">Paraná</option>
+			<option value="SC">Santa Catarina</option>
+			<option value="RS">Rio Grande do Sul</option>
+		</optgroup>
+			</select></p>
+			
+			como fazer cidade 
+			<br/><p><label for="cidade">Cidade:</label>
+			<input type="text" name="cidade" id="cidade" maxlenght="40" size="20" placeholder="Sua Cidade" list="cidades"/></p>
+			<datalist id="cidades">
+			<option value="Rio de Janeiro"></option>
+			<option value="Nova Iguaçu"></option>
+			<option value="Niterói"></option>
+			</datalist>
+				</select>
+		
 		</fieldset>
 	
 	
