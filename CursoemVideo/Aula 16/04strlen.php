090ebb915deafe87047097dfd2b5fffe10d4a3f9
Rodrigo Batista <rodrigo.batista48@etec.sp.gov.br>
2021-01-31 22:17:08 -0300

    Aula 16,funções de strings

---
+++ CursoemVideo/Aula 16/04strlen.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="_css/estilo.css"/>
+  <meta charset="UTF-8"/>
+  <title>Curso de PHP - CursoemVideo.com</title>
+</head>
+<body>
+<div>
+    <?php
+	$txt="Curso em Vídeo";
+	$tamanho = strlen($txt);
+	echo "$tamanho";
+        //essa função mostra o tamanho de caracteres do texto.
+    ?>
+</div>
+</body>
+</html>
+ 
